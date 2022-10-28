609378b377e4786ae7f3f6771770a4833264d9a5
Joao Oliveira <jmlcoliveira@gmail.com>
2022-10-28 12:08:17 +0100

    option to change transformation order

--- labs/ex12/app.js
+++ labs/ex12/app.js
@@ -20,6 +20,7 @@ let cubeCount = 0;
 let sphereCount= 0;
 
 let index = -1;
+let transformation = 0;
 
 
 function render(time)
@@ -32,6 +33,20 @@ function render(time)
 
     const uCtm = gl.getUniformLocation(program, "uCtm");
 
+    if(index != -1){
+        instances[index].px = document.getElementById("px").value;
+        instances[index].py = document.getElementById("py").value;
+        instances[index].pz = document.getElementById("pz").value;
+        instances[index].sx = document.getElementById("sx").value;
+        instances[index].sy = document.getElementById("sy").value;
+        instances[index].sz = document.getElementById("sz").value;
+        instances[index].rx = document.getElementById("rx").value;
+        instances[index].ry = document.getElementById("ry").value;
+        instances[index].rz = document.getElementById("rz").value;
+    
+        instances[index].mod = getmModel(index);
+    }
+
     for(let i = 0; i<instances.length; i++){
         gl.uniformMatrix4fv(uCtm, false, flatten(mult(mProjection, mult(mView, instances[i].mod))));
         instances[i].obj.draw(gl, program, gl.LINES);
@@ -39,28 +54,54 @@ function render(time)
 
 }
 
-function getmModel(){
-    let px = document.getElementById("px").value;
-    let py = document.getElementById("py").value;
-    let pz = document.getElementById("pz").value;
+function getmModel(i=-1){  
+    let px, py, pz, sx, sy, sz, rx, ry, rz;
+    if(i==-1){
+        px = document.getElementById("px").value;
+        py = document.getElementById("py").value;
+        pz = document.getElementById("pz").value;
+    
+        sx = document.getElementById("sx").value;
+        sy = document.getElementById("sy").value;
+        sz = document.getElementById("sz").value;
+    
+        rx = document.getElementById("rx").value;
+        ry = document.getElementById("ry").value;
+        rz = document.getElementById("rz").value;
+    }
+    else{
+        px = instances[i].px;
+        py = instances[i].py;
+        pz = instances[i].pz;
+
+        sx = instances[i].sx;
+        sy = instances[i].sy;
+        sz = instances[i].sz;
+    
+        rx = instances[i].rx;
+        ry = instances[i].ry;
+        rz = instances[i].rz;
+    }
 
     let t = translate(px, py, pz);
-
-    let sx = document.getElementById("sx").value;
-    let sy = document.getElementById("sy").value;
-    let sz = document.getElementById("sz").value;
-
     let s = scalem(sx, sy, sz);
-
-    let rx = document.getElementById("rx").value;
-    let ry = document.getElementById("ry").value;
-    let rz = document.getElementById("rz").value;
-
     let Rx = rotateX(rx);
     let Ry = rotateY(ry);
     let Rz = rotateZ(rz);
 
-    return mult(t, mult(Rz, mult(Ry, mult(Rx, s))));
+
+    switch(transformation % 5){
+        case 0:
+            return mult(t, mult(Rz, mult(Ry, mult(Rx, s))));
+        case 1:
+            return mult(t, mult(s, mult(Rz, mult(Ry, Rx))));
+        case 2:
+            return mult(s, mult(t, mult(Rz, mult(Ry, Rx))));
+        case 3:
+            return mult(Rz, mult(Ry, mult(Rx, mult(s, t))));
+        case 4: 
+            return mult(s, mult(Rz, mult(Ry, mult(Rx, t))));
+    }
 }
 
 
@@ -106,7 +147,19 @@ function setup(shaders)
     });
 
     document.getElementById("add_cube").addEventListener("click", function(){
-        instances.push({obj: CUBE, mod: getmModel()});
+        instances.push({
+            obj: CUBE, 
+            mod: getmModel(),
+            px: document.getElementById("px").value,
+            py: document.getElementById("py").value,
+            pz: document.getElementById("pz").value,
+            sx: document.getElementById("sx").value,
+            sy: document.getElementById("sy").value,
+            sz: document.getElementById("sz").value,
+            rx: document.getElementById("rx").value,
+            ry: document.getElementById("ry").value,
+            rz: document.getElementById("rz").value
+        });
         let select = document.getElementById('object_instances');
 
         var opt = document.createElement('option');
@@ -116,7 +169,19 @@ function setup(shaders)
     });
 
     document.getElementById("add_sphere").addEventListener("click", function(){
-        instances.push({obj: SPHERE, mod: getmModel()});
+        instances.push({
+            obj: SPHERE, 
+            mod: getmModel(),
+            px: document.getElementById("px").value,
+            py: document.getElementById("py").value,
+            pz: document.getElementById("pz").value,
+            sx: document.getElementById("sx").value,
+            sy: document.getElementById("sy").value,
+            sz: document.getElementById("sz").value,
+            rx: document.getElementById("rx").value,
+            ry: document.getElementById("ry").value,
+            rz: document.getElementById("rz").value,
+        });
         let select = document.getElementById('object_instances');
 
         var opt = document.createElement('option');
@@ -132,13 +197,59 @@ function setup(shaders)
         document.getElementById('object_instances').remove(instances.length);
     });
 
-    document.getElementById("object_instances").addEventListener("click", function(){
+    document.getElementById("object_instances").addEventListener("change", function(){
         let select = document.getElementById("object_instances");
         index = select.selectedIndex;
+
+        document.getElementById("px").value = instances[index].px
+        document.getElementById("py").value = instances[index].py
+        document.getElementById("pz").value = instances[index].pz
+        document.getElementById("sx").value = instances[index].sx
+        document.getElementById("sy").value = instances[index].sy
+        document.getElementById("sz").value = instances[index].sz
+        document.getElementById("rx").value = instances[index].rx
+        document.getElementById("ry").value = instances[index].ry
+        document.getElementById("rz").value = instances[index].rz
     })
 
-    document.getElementById("transform_container").addEventListener("click", function() {
+    document.getElementById("transform_container").addEventListener("change", function() {
+        instances[index].px = document.getElementById("px").value;
+        instances[index].py = document.getElementById("py").value;
+        instances[index].pz = document.getElementById("pz").value;
+        instances[index].sx = document.getElementById("sx").value;
+        instances[index].sy = document.getElementById("sy").value;
+        instances[index].sz = document.getElementById("sz").value;
+        instances[index].rx = document.getElementById("rx").value;
+        instances[index].ry = document.getElementById("ry").value;
+        instances[index].rz = document.getElementById("rz").value;
+
         instances[index].mod = getmModel();
+    })
+
+    document.addEventListener("keydown", function(event){
+        if(event.key == "t"){
+            transformation++;
+            for(let i = 0; i < instances.length; i++)
+                instances[i].mod = getmModel(i);
+
+            switch(transformation % 5){
+                case 0:
+                    document.getElementById("tranform_method").innerHTML = "S -> R -> T"
+                    break;
+                case 1:
+                    document.getElementById("tranform_method").innerHTML = "R -> S -> T"
+                    break;
+                case 2:
+                    document.getElementById("tranform_method").innerHTML = "R -> T -> S"
+                    break;
+                case 3:
+                    document.getElementById("tranform_method").innerHTML = "T -> S -> R"
+                    break;
+                case 4: 
+                    document.getElementById("tranform_method").innerHTML = "T -> R -> S"
+                    break;
+            }
+        }
     })
 
     window.requestAnimationFrame(render);
